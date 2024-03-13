4194b6122f8a15854c7051e265f670fc6288cba4
Nikhil Maguwala <102762498+nikhilmaguwala-fau@users.noreply.github.com>
2024-03-13 18:17:32 +0000

    todolist first prototype

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,61 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+	const [task, setTask] = useState('')
+	const [todos, setTodos] = useState([])
+
+	// Function to check enter is pressed or not... if yes add task to list after validation
+	const handleKeyPress = (event) => {
+		if(event.key === 'Enter') {
+			if(task === '') {
+				alert('Task can not be empty!')
+			} else {
+				addTask(task)
+			}
+		}
+	}
+
+	const addTask = (newTask) => {
+		setTodos([...todos, newTask])
+		setTask('')
+	}
+
+	const deleteTask = (task) => {
+		// JS Filter Method
+		const tempTodos = todos.filter(todo => todo !== task)
+		setTodos(tempTodos)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className='container'>
+			<input 
+				value={task}
+				onChange={event => setTask(event.target.value)}
+				onKeyDown={handleKeyPress}
+				placeholder='Enter new task...'
+			/>
+			<ul>
+				{todos.map( todo => {
+						return (
+							<li key={todo}>
+								{todo} <i 
+									className="far fa-trash-alt"
+									onClick={() => deleteTask(todo)}
+									style={{ cursor: 'pointer' }}
+								></i>
+							</li>
+						)
+				})}
+			</ul>
+			{todos.length === 0 ? (
+				<h5>No Task Exist. Add new task...</h5>
+			): (
+				<h5>{todos.length} tasks left</h5>
+			)}
 		</div>
-	);
+	)
 };
 
 export default Home;
